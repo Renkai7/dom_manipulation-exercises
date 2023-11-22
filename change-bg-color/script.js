a9e50eae052e80451373700055156cc79f2413a8
Michael Spencer <ms53150@gmail.com>
2023-11-21 20:13:39 -0500

    add background color change exercise

---
+++ change-bg-color/script.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("change-color-button");
+const colorChangeDiv = document.getElementById("color-change-div");
+
+const getRandomColor = () => {
+	let color = "#";
+	const alphaNum = "0123456789ABCDEF";
+	for (let i = 0; i < 6; i++) {
+		color += alphaNum[Math.floor(Math.random() * 16)];
+	}
+
+	return color;
+};
+
+button.addEventListener("click", () => {
+	const randomColor = getRandomColor();
+	colorChangeDiv.style.backgroundColor = randomColor;
+});
